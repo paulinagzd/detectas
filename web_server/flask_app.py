c486febe28430d4fc60e1ccb08ee6053a0bfab17
Vishal Singh <tmibvishal@gmail.com>
2021-02-13 15:34:53 +0530

    add route classify_image

--- web_server/flask_app.py
+++ web_server/flask_app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
@@ -12,6 +14,18 @@ def index():
 @app.route("/multi/<int:num>", methods=["GET"])
 def get_multiply10(num):
 	return jsonify({"route": f"/multi/{num}", "method": "GET", "ans": num*10})
+
+# curl -F "file=@image.jpg" http://localhost:5000/
+@app.route("/classify_image", methods=["POST"])
+def classify_image():
+	# TODO: add a try catch block here
+    img = Image.open(request.files['file'])
+    # print(img.size)
+    numpy_img = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3)
+    print(numpy_img.shape)
+    print(numpy_img)
+    # classes = {0: "autistic", 1: "non_autistic", -1: "error_read_description"}
+    return jsonify({"route": f"/classify_image", "method": "POST", "class": -1, "description": "Can't find face. Please recapture the image"})
 
 if __name__ == "__main__":
     app.run(debug=True)
